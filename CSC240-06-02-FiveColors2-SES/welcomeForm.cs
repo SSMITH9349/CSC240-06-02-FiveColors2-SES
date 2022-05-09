0ba8ac30fa9a81373b1485babe014edaeffaf052
Sara Smith <SSMITH9349@my.sullivan.edu>
2022-05-09 15:11:47 -0500

    Created Label and RadioButton

---
+++ CSC240-06-02-FiveColors2-SES/welcomeForm.cs
@@ -0,0 +1,30 @@
+namespace CSC240_06_02_FiveColors2_SES
+{
+    public partial class welcomeForm : Form
+    {
+        public welcomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void welcomeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GreenCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
